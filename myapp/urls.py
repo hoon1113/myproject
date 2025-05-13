7a38871726b21f10206102e16bb78e0b8fc7b1da
hoon1113 <tree4748@naver.com>
2025-05-13 17:01:13 +0900

    내가 몇가지 수정

--- myapp/urls.py
+++ myapp/urls.py
@@ -6,9 +6,7 @@ urlpatterns = [
     path('',views.login, name='login'),
     path('home/', views.home, name='home'),
     path('busmap/', views.busmap, name='busmap'),
-
     path('map/', views.map, name='map'),
-
     path('map/', map_view, name='map'),
 
 ]
